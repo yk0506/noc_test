e04d1caf90744b44b5818caaea95d3d7a1f745bf
rachel <rachel.yang@companywe.co.kr>
2016-06-22 14:33:19 +0900

    power plant init

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('power-plant')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout) {
+    var vm = this;
+
+  }
+})();
