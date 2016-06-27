8fc2435f9aa5a1aac7d56d382de9ae4bb7c031fb
rachel <rachel.yang@companywe.co.kr>
2016-06-27 22:27:32 +0900

    ADDED state2

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,9 +8,21 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('state1', {
         url: '/',
         templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('state2', {
+        url: '/state2',
+        templateUrl: 'app/main/main5.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('state2-1', {
+        url: '/state2-1',
+        templateUrl: 'app/main/main6.html',
         controller: 'MainController',
         controllerAs: 'main'
       });
