f1c199eabd0453994b2d0f6c048c6937d3923816
rachel <rachel.yang@companywe.co.kr>
2016-10-06 11:11:24 +0900

    ADDED zoom.js

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -11,48 +11,13 @@
     vm._ = _;
 
      var settings = {
-      // zoomed size relative to the container element
-      // 0.0-1.0
-      // targetsize: 0.9,
-
-      // scale content to screen based on their size
-      // "width"|"height"|"both"
-      // scalemode: "both",
-
-      // animation duration
-      // duration: 450,
-
-      // easing of animation, similar to css transition params
-      // "linear"|"ease"|"ease-in"|"ease-out"|"ease-in-out"|[p1,p2,p3,p4]
-      // [p1,p2,p3,p4] refer to cubic-bezier curve params
-      // easing: "ease",
-
-      // use browser native animation in webkit, provides faster and nicer
-      // animations but on some older machines, the content that is zoomed
-      // may show up as pixelated.
-      // nativeanimation: true,
-
-      // root element to zoom relative to
-      // (this element needs to be positioned)
-      // root: $(document.body),
       root: $(".zoomViewport"),
-
       // show debug points in element corners. helps
       // at debugging when zoomooz positioning fails
-      debug: true,
-
-      // this function is called with the element that is zoomed to in this
-      // when animation ends
-      // animationendcallback: null,
-
+      debug: false,
       // this specifies, that clicking an element that is zoomed to zooms
       // back out
       closeclick: true
-
-      // don't reset scroll before zooming. less jaggy zoom starts and ends on
-      // mobile browsers, but causes issues when zooming to elements when scrolled
-      // to a specific distance in document, typically around 2000px on webkit.
-      // preservescroll: false
     };
 
     $(document).ready(function() {
