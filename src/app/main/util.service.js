58dbae1911b992e1eb201002b60bf9b545026c63
허걸 <dkfdk11@nate.com>
2017-01-19 19:49:04 +0900

    DR 중앙 상단 그래프 둥글게 표현 제거

--- src/app/main/util.service.js
+++ src/app/main/util.service.js
@@ -88,8 +88,8 @@
           data: {
             x: vm.timeX[0],
             xFormat:'%H:%M',
-            columns: [vm.timeX, cbl, watt],
-            type: 'spline'  //Line 둥글게
+            columns: [vm.timeX, cbl, watt]
+            //,type: 'spline'  //Line 둥글게
           },
           grid: { //점선
             x: {
