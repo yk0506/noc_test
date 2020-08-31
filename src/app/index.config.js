86afd4fde823fbe6d0bf2253478c0c56aced79f7
puchi <yunkyung0506@gmail.com>
2020-08-31 11:47:11 +0900

    로그인처리 추가

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -11,4 +11,15 @@
     $logProvider.debugEnabled(true);
   }
 
+
+  /* 로그인 후 접속할 수 있도록 쿠키 체크 로직 추가 */
+  var getCookie = function(name) {
+      var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+      return value? value[2] : null;
+  };
+
+  if( getCookie("westpoleid") == null ) {
+    window.location.href = "http://dev.westpole.biz/login";
+  }
+
 })();
