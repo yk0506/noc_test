fefbcd52cace797392b3dd2d54c93f7b87769b47
puchi <yunkyung0506@gmail.com>
2020-08-31 12:00:25 +0900

    로그인처리 추가 - 특정 도메인 포함시에만 동작하도록 변경

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -13,13 +13,16 @@
 
 
   /* 로그인 후 접속할 수 있도록 쿠키 체크 로직 추가 */
+  /* 특정 도메인 경로를 포함할 때만 동작하도록 함 */
+
+  var curr_loc = window.location.href;
   var getCookie = function(name) {
-      var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-      return value? value[2] : null;
-  };
+        var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+        return value? value[2] : null;
+    };
 
-  if( getCookie("westpoleid") == null ) {
-    window.location.href = "http://dev.westpole.biz/login";
+  if(curr_loc.indexOf("westpole") !== -1
+      && getCookie("westpoleid") == null ) {
+        window.location.href = "http://dev.westpole.biz/login";
   }
-
 })();
